dfb437560ffdf683ce8d1fc2d2759fc1725828bc
Samuel Koch <samuel.koch08@gmail.com>
2022-11-18 17:45:30 -0700

    finished productsrouter, beginning testing

--- src/api/productsRouter.js
+++ src/api/productsRouter.js
@@ -1,41 +1,85 @@
 const express = require('express');
-const { getAllProducts, createProduct } = require('../db');
+const { updateProduct, deleteProduct } = require('../../db/products');
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  getProductById,
+  deleteProduct
+} = require('../db');
+
 const { requireAdmin } = require('./utils');
 
 const productsRouter = express.Router();
 
-// const { getAllProducts, createProduct, editProduct, getProductById, getProductsByCategory, getProductByName } and whatever else we need
 productsRouter.get('/', async (req, res, next) => {
   try {
     const allProducts = await getAllProducts();
     res.send(allProducts)
-  } catch(err) {
+  } catch (err) {
     console.log('productsRouter.get-productsRouter.js FAILED', err)
   }
 });
 
 productsRouter.post('/create-product', requireAdmin, async (req, res, next) => {
   try {
-    const product = await createProduct(requireAdmin)
-    if(product) {
+    const product = await createProduct({
+      title,
+      description,
+      price,
+      count
+    })
+
+    if (product) {
       res.send(product)
-    } else {
-      next({
-        name: 'CreateProductError',
-        message: 'Error creating product'
-      })
     }
-  } catch(err) {
+  } catch (err) {
     console.log('productsRouter.post FAILED', err)
   }
 });
 
-productsRouter.patch('/edit-product', (req, res, next) => {
-  res.send('Edit product admin only')
-})
+productsRouter.patch('/:productId', requireAdmin, async (req, res, next) => {
+  const { productId } = req.params;
+  const { title, description, price, count } = req.body;
+  const updateFields = {};
 
-productsRouter.delete('/edit-product', (req, res, next) => {
-  res.send('Delete product admin only')
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (description) {
+    updateFields.description = description
+  }
+
+  if (price) {
+    updateFields.price = price;
+  }
+
+  if (count) {
+    updateFields.count = count;
+
+  }
+  try {
+    const updatedProduct = await updateProduct(productId, updateFields)
+
+    if (updatedProduct) {
+      res.send(updatedProduct)
+    }
+  } catch (err) {
+    console.log('productsRouter.patch FAILED', err)
+  }
+});
+
+productsRouter.delete('/:productId', requireAdmin, async (req, res, next) => {
+  try {
+    const product = await getProductById(productId)
+    const deletedProduct = await deleteProduct(product.id, { active: false })
+
+    res.send(deletedProduct)
+    console.log('This product was deleted.')
+  } catch (err) {
+    console.log('productRouter.delete FAILED', err)
+  }
 });
 
 module.exports = productsRouter;
